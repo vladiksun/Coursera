fc127836e829a3cf1a95f049723497c534d074eb
Vladislav Bondarchuk <bondarchuk@haulmont.com>
2017-09-04 18:22:49 +0400

    first commit

---
+++ Parallel Programming in Java/fork-and-join-examples/src/main/java/com/logicbig/example/CalcUtil.java
@@ -0,0 +1,16 @@
+package com.logicbig.example;
+
+import java.math.BigInteger;
+
+public class CalcUtil {
+    
+    public static BigInteger calculateFactorial (BigInteger input) {
+        BigInteger factorial = BigInteger.ONE;
+        for (BigInteger i = BigInteger.ONE;
+             i.compareTo(input) <= 0;
+             i = i.add(BigInteger.ONE)) {
+            factorial = factorial.multiply(i);
+        }
+        return factorial;
+    }
+}
